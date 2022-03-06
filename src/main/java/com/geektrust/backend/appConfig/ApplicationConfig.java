fb0a1ecc8616bffba4026284895dc9561e34b1fd
Akash Kumar Sinha <AkashKumarSinha007@gmail.com>
2022-03-05 16:20:36 -1200

    Added initial boiler late changes

---
+++ src/main/java/com/geektrust/backend/appConfig/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.geektrust.backend.appConfig;
+
+import com.geektrust.backend.commands.CommandInvoker;
+
+public class ApplicationConfig {
+    public CommandInvoker getCommandInvoker(){
+        return null;
+    }
+}
---
+++ src/main/java/com/geektrust/backend/commands/CommandInvoker.java
@@ -0,0 +1,8 @@
+package com.geektrust.backend.commands;
+
+import java.util.List;
+
+public class CommandInvoker {
+    public void executeCommand(String commandName, List<String> tokens) {
+    }
+}
